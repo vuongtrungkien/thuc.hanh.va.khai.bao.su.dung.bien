43353b353459f1cc5d0d5045249616fb20861191
vuongtrungkien <nobitaa6@gmail.com>
2019-07-31 22:02:04 +0800

    bai thuc hanh khai bao va su dung bien

---
+++ bai 2/jsbai2.js
@@ -0,0 +1,5 @@
+let width = 20
+let height = 10
+let area = width * height
+
+document.write("dien tich hinh chu nhat co chieu dai 10 chieu rong 20 la :" + area)
---
+++ bai 3/jsbai3.js
@@ -0,0 +1,16 @@
+let inputa;
+let inputb;
+
+inputa = prompt("nhap so a")
+inputb = prompt("nhap so b")
+
+let a =parseInt(inputa);
+let b = parseInt(inputb);
+
+    if (a%b ==0) {
+        alert("a chia het cho b")
+    }else {
+        alert("a khong chia het cho b")
+}
+
+
